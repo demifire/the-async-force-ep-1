dfbc2d3a2f670475d52fd1fb869b822bc7e34ba5
Chaz Kondo <Chaz@Macbook-Pro.local>
2018-07-31 23:47:44 -1000

    Another test

--- js/app.js
+++ js/app.js
@@ -60,7 +60,7 @@ document.addEventListener('click',function starwars() {
         setTimeout(function(){
             document.getElementById("scroll-container").style.display = "none";
             getData();
-         }, 1000);
+         }, 96000);
     };
 });
 
@@ -138,7 +138,10 @@ function newReq(url){
     return newRequest
 }
 
-let stylesheet = document.styleSheets[0];
-stylesheet.disabled = true;
+// let stylesheet = document.styleSheets[0];
+// stylesheet.disabled = true;
 
+for ( i=0; i<document.styleSheets.length; i++) {
+    void(document.styleSheets.item(i).disabled=true);
+}
 }
